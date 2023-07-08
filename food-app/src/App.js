23684b0ff576e760b051dec7ab11e18ba0928f18
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2023-07-09 01:57:14 +0530

    completed till crousel

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -8,6 +8,10 @@ import {
   
 } from "react-router-dom";
 import Login from './Screens/Login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 
 function App() {
--- food-app/src/components/Card.js
+++ food-app/src/components/Card.js
@@ -5,7 +5,7 @@ export default function Card() {
         <div>
             <div>
                 <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                    <img src="..." className="card-img-top" alt="..." />
+                    <img src="https://www.100daysofrealfood.com/wp-content/uploads/2012/02/pesto.jpg" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">itle and make up the bulk of the card's content.</p>
